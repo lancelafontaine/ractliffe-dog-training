acebcbfe5b7f8b4f401db5ead51b9f2074aef8e1
Lance Lafontaine <lance.lafontaine92@gmail.com>
2016-09-20 23:27:31 -0400

    Fixes same Google Maps issue again, was not resolved initially.

--- js/maps.js
+++ js/maps.js
@@ -1,10 +1,13 @@
 (function () {
-  
+
 function initGoogleMaps() {
   var map = new google.maps.Map(document.getElementById("map_canvas"), {
     center:new google.maps.LatLng(45.3998, -74.1378),
     zoom: 11,
     mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  $('#contact, #res-contact').click(function () {
+    google.maps.event.trigger(map, 'resize');
   });
 }
 
@@ -12,8 +15,5 @@ $(window).load(function () {
   initGoogleMaps();
 });
 
-$('#contact, #res-contact').click(function () {
-  google.maps.event.trigger(map, 'resize');
-})
 
 })();
