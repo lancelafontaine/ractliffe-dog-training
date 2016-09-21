1035937280fef47711c26027f6994e7dbdd37a51
Lance Lafontaine <lance.lafontaine92@gmail.com>
2016-09-21 13:27:12 -0400

    Fixes Google Maps zoom

--- js/maps.js
+++ js/maps.js
@@ -4,6 +4,8 @@ function initGoogleMaps() {
   var map = new google.maps.Map(document.getElementById("map_canvas"), {
     center:new google.maps.LatLng(45.3998, -74.1378),
     zoom: 11,
+    maxZoom: 11,
+    minZoom: 11,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
   $('#contact, #res-contact').click(function () {
